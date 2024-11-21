b38881a3f3c5e54f5fa6b8ffb14fd12872a3d687
Yunseok <watemark1017@gmail.com>
2024-11-21 21:19:42 +0900

    feat: 버튼 전환 상태기능 추가

--- src/components/MyPage/MenuButton.jsx
+++ src/components/MyPage/MenuButton.jsx
@@ -1,12 +1,27 @@
+import { useState, useEffect } from 'react';
 import cn from '../../utils/cn';
 
-export default function MenuButton({ icon, text }) {
+export default function MenuButton({ icon, text, buttonClicked, setButtonClicked}) {
+  const[buttonColorOn, setButtonColorOn] = useState(false);
+  useEffect(() => {
+    if(buttonClicked === text) {
+      setButtonColorOn(true);
+    } else {
+      setButtonColorOn(false);
+    }
+  }, [buttonClicked, text]);
+
   return (
-    <button className={cn(
-      "relative rounded-lg border-2 border-gray-400 p-2 mx-2 my-4 text-2xl text-black",
-      "hover:bg-theme-green hover:border-theme-green hover:text-white",
-      "transition duration-200 flex items-center justify-center"
-    )}>
+    <button 
+        onClick={() => setButtonClicked(text)}
+        className={cn(
+          "relative rounded-lg border-2 p-2 mx-6 my-4 text-2xl",
+          buttonColorOn ? "bg-theme-green border-theme-green text-white" : 
+          "border-gray-400 bg-white text-black",
+          "hover:bg-theme-green hover:border-theme-green hover:text-white",
+          "transition duration-200 flex items-center justify-center"
+      )}
+    >
       {/* 아이콘: 좌측 고정 */}
       <span className="absolute left-4 flex items-center">
         {icon}
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,15 +1,34 @@
+import { useState } from 'react';
 import cn from '../utils/cn';
 import MenuButton from '../components/MyPage/MenuButton';
 import { GoStack, GoBookmark, GoFile } from "react-icons/go";
 
 export default function MyPage() {
+  const[buttonClicked, setButtonClicked] = useState('');
   return (
     <div className="flex flex-col">
-      <div className="border-r-2 border-black flex flex-col justify-top max-w-xs min-h-screen">
+      <div className={cn(
+        "border-r-2 border-black flex flex-col justify-top max-w-xs min-h-screen"
+      )}>
         <p className='mb-2'></p>
-        <MenuButton icon={<GoStack />} text="과제 현황" />
-        <MenuButton icon={<GoBookmark />} text="뱃지" />
-        <MenuButton icon={<GoFile />} text="포트폴리오" />
+        <MenuButton 
+          icon={<GoStack />}
+          text="과제 현황"
+          buttonClicked={buttonClicked}
+          setButtonClicked={setButtonClicked}
+        />
+        <MenuButton
+          icon={<GoBookmark />}
+          text="뱃지"
+          buttonClicked={buttonClicked}
+          setButtonClicked={setButtonClicked}
+        />
+        <MenuButton
+          icon={<GoFile />}
+          text="포트폴리오" 
+          buttonClicked={buttonClicked}
+          setButtonClicked={setButtonClicked}
+        />
       </div>
     </div>
   );
