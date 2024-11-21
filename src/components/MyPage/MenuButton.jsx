d7a58b9a7575fd734faf0553c420be1f14ba0583
Yunseok <watemark1017@gmail.com>
2024-11-21 22:16:42 +0900

    style: 가로폭에 따라 크기 조절

--- src/components/MyPage/MenuButton.jsx
+++ src/components/MyPage/MenuButton.jsx
@@ -15,19 +15,18 @@ export default function MenuButton({ icon, text, buttonClicked, setButtonClicked
     <button 
         onClick={() => setButtonClicked(text)}
         className={cn(
-          "relative rounded-lg border-2 p-2 mx-6 my-4 text-2xl",
-          buttonColorOn ? "bg-theme-green border-theme-green text-white" : 
-          "border-gray-400 bg-white text-black",
+          "relative rounded-lg border-2 p-2 mx-6 my-4 w-40 lg:w-56",
+          buttonColorOn ? "bg-theme-green border-theme-green text-white" : "border-gray-400 bg-white text-black",
           "hover:bg-theme-green hover:border-theme-green hover:text-white",
           "transition duration-200 flex items-center justify-center"
       )}
     >
       {/* 아이콘: 좌측 고정 */}
-      <span className="absolute left-4 flex items-center">
+      <span className="text-xl lg:text-2xl absolute left-4 flex items-center">
         {icon}
       </span>
       {/* 텍스트: 중앙 정렬 */}
-      <span>
+      <span className='text-lg lg:text-2xl'>
         {text}
       </span>
     </button>
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
 import cn from '../utils/cn';
 import MenuButton from '../components/MyPage/MenuButton';
+import TaskPage from '../components/MyPage/Tasks/TaskPage';
 import { GoStack, GoBookmark, GoFile } from "react-icons/go";
 
 export default function MyPage() {
-  const[buttonClicked, setButtonClicked] = useState('');
+  const[buttonClicked, setButtonClicked] = useState('과제 현황');
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-row h-screen">
       <div className={cn(
-        "border-r-2 border-black flex flex-col justify-top max-w-xs min-h-screen"
+        "border-r-2 border-black flex flex-col justify-top min-w-xs min-h-screen"
       )}>
         <p className='mb-2'></p>
         <MenuButton 
@@ -29,6 +30,9 @@ export default function MyPage() {
           buttonClicked={buttonClicked}
           setButtonClicked={setButtonClicked}
         />
+      </div>
+      <div className="flex-1 ">
+        <TaskPage />
       </div>
     </div>
   );
