9d65e75c7a5e1219cfea4b62d4718fad12bee88e
Yunseok <watemark1017@gmail.com>
2024-11-24 04:25:31 +0900

    fix: 드롭다운 스타일 수정

--- src/components/Header/CommonHeader.jsx
+++ src/components/Header/CommonHeader.jsx
@@ -8,28 +8,26 @@ import Logo from '../../assets/Logo.svg';
 import useProfileData from '../../hooks/useProfileData';
 import DefaultProfile from '../../assets/icons/ProfileLg.svg';
 
-const DropdownMenu = ({ showDropdown, setShowDropdown, menuItems, style, zIndex = 10 }) => {
+const DropdownMenu = ({ showDropdown, menuItems, style, zIndex = 10 }) => {
   return (
-    showDropdown && (
-      <div
-        className="h-40 w-[10%] rounded-b-2xl pt-5 absolute transform -translate-x-1/2 text-center"
-        style={{ ...style, zIndex }}
-        onMouseEnter={() => setShowDropdown(true)}
-        onMouseLeave={() => setShowDropdown(false)}
-      >
-        <div className="font-extrabold text-white space-y-5 text-lg">
-          {menuItems.map((item, index) => (
-            <h1
-              key={index}
-              className="cursor-pointer"
-              onClick={() => item.onClick()}
-            >
-              {item.label}
-            </h1>
-          ))}
-        </div>
+    <div
+      className={`absolute top-11 left-1/2 transform -translate-x-1/2 w-40 rounded-b-2xl shadow-md bg-white transition-all duration-200 ${
+        showDropdown ? 'opacity-100 scale-100 pointer-events-auto' : 'opacity-0 scale-95 pointer-events-none'
+      }`}
+      style={{ ...style, zIndex }}
+    >
+      <div className="font-semibold text-black space-y-2 py-3 px-3 text-center">
+        {menuItems.map((item, index) => (
+          <h1
+            key={index}
+            className="cursor-pointer hover:text-theme-red text-base md:text-lg"
+            onClick={item.onClick}
+          >
+            {item.label}
+          </h1>
+        ))}
       </div>
-    )
+    </div>
   );
 };
 
@@ -38,14 +36,14 @@ export default function CommonHeader() {
   const ProfileData = useProfileData(); // ProfileData 초기값은 {}
   const [isMobile, setIsMobile] = useState(false);
   const [openMobileMenu, setOpenMobileMenu] = useState(false);
-  const [showCommunityDropdown, setShowCommunityDropdown] = useState(false); // Community 드롭다운
-  const [showMyPageDropdown, setShowMyPageDropdown] = useState(false); // My Page 드롭다운
-
-  const handleLogoClick = () => navigate('/');
+  const [showDropdown, setShowDropdown] = useState({
+    community: false,
+    myPage: false,
+  });
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth < 768); // 모바일 여부 확인
+      setIsMobile(window.innerWidth < 768);
     };
 
     handleResize();
@@ -57,7 +55,7 @@ export default function CommonHeader() {
     <header className="flex justify-between items-center bg-theme-green px-4">
       {/* 로고 */}
       <div
-        onClick={handleLogoClick}
+        onClick={() => navigate('/')}
         className="flex flex-row gap-3 cursor-pointer p-2"
       >
         <img src={Logo} alt="logo" className="h-10" />
@@ -66,46 +64,12 @@ export default function CommonHeader() {
         </h1>
       </div>
 
-      {/* Community 드롭다운 */}
-      <DropdownMenu
-        showDropdown={showCommunityDropdown}
-        setShowDropdown={setShowCommunityDropdown}
-        menuItems={[
-          { label: '멘토링 요청', onClick: () => navigate('/Ourservice') },
-          { label: '나의 멘토링', onClick: () => navigate('/community/나의멘토링') }
-        ]}
-        style={{
-          top: '50px',
-          left: 'calc(86% - 50px)', // 화면 크기와 관계 없이 정확히 Community 아래에 표시
-          background: "linear-gradient(to bottom, white 7.9%, #90A8C0 1%)"
-        }}
-        zIndex={20} // Community 드롭다운의 z-index 높게 설정
-      />
-
-      {/* My Page 드롭다운 */}
-      <DropdownMenu
-        showDropdown={showMyPageDropdown}
-        setShowDropdown={setShowMyPageDropdown}
-        menuItems={[
-          { label: '나의 과제', onClick: () => navigate('MyPage/나의과제') },
-          { label: '단풍 뱃지 수집', onClick: () => navigate('MyPage/단풍뱃지수집') },
-          { label: '포트폴리오', onClick: () => navigate('MyPage/포트폴리오') }
-        ]}
-        style={{
-          top: '50px',
-          left: 'calc(87% + 100px)', // 화면 크기와 관계 없이 정확히 My Page 아래에 표시
-          background: "linear-gradient(to bottom, white 7.9%, #90A8C0 1%)"
-        }}
-        zIndex={15} // My Page 드롭다운의 z-index 낮게 설정
-      />
-
       {/* 네비게이션 */}
       {isMobile ? (
         <>
           <button onClick={() => setOpenMobileMenu(true)}>
             <HiMenu className="text-white text-3xl" />
           </button>
-          {/* 모바일 메뉴 */}
           <MobileMenu
             openMobileMenu={openMobileMenu}
             setOpenMobileMenu={setOpenMobileMenu}
@@ -113,7 +77,7 @@ export default function CommonHeader() {
           />
         </>
       ) : (
-        <div className='flex flex-row justify-end items-center'>
+        <div className="flex flex-row justify-end items-center">
           {ProfileData?.profile ? (
             <img
               src={ProfileData.profile || DefaultProfile}
@@ -122,43 +86,77 @@ export default function CommonHeader() {
               onClick={() => navigate('/profile')}
             />
           ) : (
-            <div className="w-12 h-12 rounded-full bg-gray-300 animate-pulse"></div>
+            <div className="w-10 lg:w-12 h-10 lg:h-12 rounded-full bg-gray-300 animate-pulse"></div>
           )}
           <nav
             className={cn(
-              'flex flex-row rounded-full bg-white text-[#142D6F] text-sm lg:text-lg',
-              'p-3 m-3 gap-8 lg:gap-16 xl:gap-20 px-12 lg:px-20'
+              'flex flex-row rounded-full bg-white text-black text-sm lg:text-lg',
+              'p-2 md:p-3 m-3 gap-4 lg:gap-16 xl:gap-20 px-12 lg:px-20'
             )}
           >
             <button onClick={() => navigate('/home')}>Home</button>
             <button onClick={() => navigate('/ourservice')}>Our Service</button>
 
-            {/* Community 메뉴 - 드롭다운 */}
+            {/* Community 메뉴 */}
             <div
-              className='flex items-center gap'
-              onMouseEnter={() => setShowCommunityDropdown(true)} // 마우스가 올려졌을 때
-              onMouseLeave={() => setShowCommunityDropdown(false)} // 마우스가 떠났을 때
+              className="relative flex items-center group"
             >
-              <button onClick={() => navigate('/community')}
-                className="flex items-center justify-center w-24 h-2 rounded-full"
-              >
+              <button className="flex items-center justify-center w-28 h-8">
                 Community
-                <BiChevronDown className="text-2xl text-[#142D6F] mt-1" />
+                <BiChevronDown className="text-2xl text-black ml-1" />
               </button>
+              <div
+                className={`absolute top-full left-1/2 transform -translate-x-1/2 w-40 rounded-b-2xl shadow-md bg-white transition-all duration-200 opacity-0 scale-95 pointer-events-none group-hover:opacity-100 group-hover:scale-100 group-hover:pointer-events-auto`}
+              >
+                <div className="font-semibold text-black space-y-2 py-3 px-3 text-center">
+                  <h1
+                    className="cursor-pointer hover:text-theme-red text-base md:text-lg"
+                    onClick={() => navigate('/community/request')}
+                  >
+                    멘토링 요청
+                  </h1>
+                  <h1
+                    className="cursor-pointer hover:text-theme-red text-base md:text-lg"
+                    onClick={() => navigate('/community/mymentoring')}
+                  >
+                    나의 멘토링
+                  </h1>
+                </div>
+              </div>
             </div>
 
-            {/* My page 메뉴 - 드롭다운 */}
+            {/* My Page 메뉴 */}
             <div
-              className='flex items-center gap'
-              onMouseEnter={() => setShowMyPageDropdown(true)} // 마우스가 올려졌을 때
-              onMouseLeave={() => setShowMyPageDropdown(false)} // 마우스가 떠났을 때
+              className="relative flex items-center group"
             >
-              <button onClick={() => navigate('/mypage')}
-                className="flex items-center justify-center w-24 h-2 rounded-full"
-              >
+              <button className="flex items-center justify-center w-28 h-8">
                 My Page
-                <BiChevronDown className="text-2xl text-[#142D6F] mt-1" />
+                <BiChevronDown className="text-2xl text-black ml-1" />
               </button>
+              <div
+                className={`absolute top-full left-1/2 transform -translate-x-1/2 w-40 rounded-b-2xl shadow-md bg-white transition-all duration-200 opacity-0 scale-95 pointer-events-none group-hover:opacity-100 group-hover:scale-100 group-hover:pointer-events-auto`}
+              >
+                <div className="font-semibold text-black space-y-2 py-3 px-3 text-center">
+                  <h1
+                    className="cursor-pointer hover:text-theme-red text-base md:text-lg"
+                    onClick={() => navigate('/mypage/mytask')}
+                  >
+                    과제 현황
+                  </h1>
+                  <h1
+                    className="cursor-pointer hover:text-theme-red text-base md:text-lg"
+                    onClick={() => navigate('/mypage/badge')}
+                  >
+                    뱃지
+                  </h1>
+                  <h1
+                    className="cursor-pointer hover:text-theme-red text-base md:text-lg"
+                    onClick={() => navigate('/mypage/portfolio')}
+                  >
+                    포트폴리오
+                  </h1>
+                </div>
+              </div>
             </div>
           </nav>
         </div>
