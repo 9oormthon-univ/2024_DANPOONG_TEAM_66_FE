aec66f2468b61cf1115c806d58ff7d421e2333c3
Yunseok <watemark1017@gmail.com>
2024-11-24 02:07:54 +0900

    fix: 메인페이지 헤더 수정

--- src/components/Header/MainPageHeader.jsx
+++ src/components/Header/MainPageHeader.jsx
@@ -137,10 +137,6 @@ export default function MainPageHeader() {
                 'p-5 gap-12 mr-4'
               )}
             >
-
-              <button onClick={() => navigate('/home')} className="flex items-center justify-center w-24 h-2 rounded-full ml-10">Home</button>
-              <button onClick={() => navigate('/register')} className="flex items-center justify-center w-24 h-2 rounded-full">서비스 소개</button>
-
               <button 
                 onClick={() => navigate('/')}
                 className="flex items-center justify-center w-24 h-2 rounded-full ml-10"
